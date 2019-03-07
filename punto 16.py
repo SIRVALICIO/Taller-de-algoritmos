5543ff22225eae41c3d5aa12be3a5d4448535c75
valquero779 <andresserrano2002@hotmail.com>
2019-03-07 07:48:54 -0500

    mi primer commit

---
+++ punto 16.py
@@ -0,0 +1,5 @@
+a=float(input("valor de la aceleracion: "))
+t=float(input("valor del tiempo en seg: "))
+vo=0
+vf=vo+a*t
+print("El valor de la distancia recorrida es: ",vf)
---
+++ punto 17.py
@@ -0,0 +1,4 @@
+m=float(input("ingresar la masa del objeto: "))
+c=299792458
+e=m*(c**2)
+print("La energia de ese objeto es: ",e)

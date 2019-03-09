f3c29609963d6b6926335365bb06bbc9c5741bab
valquero779 <andresserrano2002@hotmail.com>
2019-03-09 18:28:56 -0500

    primer commit

---
+++ punto 41.py
@@ -0,0 +1,23 @@
+
+for i in range(0,11):
+    if i==1:
+        print("uno")
+    if i == 2:
+        print("dos")
+    if i == 3:
+        print("tres")
+    if i == 4:
+        print("cuatro")
+    if i==5:
+        print("cinco")
+    if i == 6:
+        print("seis")
+    if i == 7:
+        print("siete")
+    if i == 8:
+        print("ocho")
+    if i==9:
+        print("nueve")
+    if i == 10:
+        print("diez")
+

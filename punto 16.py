10cc37c529154953b1d39ffe58c33fe7eaf6f6f3
valquero779 <andresserrano2002@hotmail.com>
2019-03-09 18:45:57 -0500

    corecciones

--- punto 16.py
+++ punto 16.py
@@ -2,4 +2,4 @@ a=float(input("valor de la aceleracion: "))
 t=float(input("valor del tiempo en seg: "))
 vo=0
 vf=vo+a*t
-print("El valor de la distancia recorrida es: ",vf)
+print("la velocida final del objeto es: ",vf)
